a089f3399c231d7027c12e98819b035284f55afb
maxsim86 <maxsim86@gmail.com>
2022-04-24 08:16:41 +0800

    write code in temujanji/user/booking_done.html

--- temujanji/forms.py
+++ temujanji/forms.py
@@ -12,43 +12,53 @@ class TukarInputStyle(forms.Form):
             input_type = self.fields[field].widget.input_type
             classes = self.fields[field].widget.attrs.get("class")
             if classes is not None:
-                classes += "form-check-input" if input_type == "checkbox" else "form control flatpickr-input"
+                classes += (
+                    "form-check-input"
+                    if input_type == "checkbox"
+                    else "form control flatpickr-input"
+                )
             else:
-                classes = 'form-check -input' if input_type == 'checkbox' else "form contrl flatpickr-input"
-                self.fields[field].widget.attrs.update({'class':classes})
-                    
+                classes = (
+                    "form-check -input"
+                    if input_type == "checkbox"
+                    else "form contrl flatpickr-input"
+                )
+                self.fields[field].widget.attrs.update({"class": classes})
+
 
 class TempahanTarikhForm(TukarInputStyle):
-    tarikh = forms.DateField(required=True,)
+    tarikh = forms.DateField(
+        required=True,
+    )
+
 
 class TempahanMasaForm(TukarInputStyle):
     masa = forms.TimeField(widget=forms.HiddenInput())
 
+
 class TempahanPlangganForm(TukarInputStyle):
     nama_pengguna = forms.CharField(max_length=250)
-    email_pengguna =  forms.EmailField()
+    email_pengguna = forms.EmailField()
     telefon_pengguna = forms.CharField(required=False, max_length=10)
 
+
 class TempahanSettingsForm(TukarInputStyle, forms.ModelForm):
-    masa_mula = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
-    masa_tamat = forms.TimeField(widget=forms.TimeInput(format='%H:%M' ))
+    masa_mula = forms.TimeField(widget=forms.TimeInput(format="%H:%M"))
+    masa_tamat = forms.TimeField(widget=forms.TimeInput(format="%H:%M"))
 
     def clean(self):
-        if 'masa_tamat' in self.cleaned_data and 'masa_mula' in self.cleaned_data:
-            if self.cleaned_data['masa_tamat'] <= self.cleaned_data['masa_mula']:
+        if "masa_tamat" in self.cleaned_data and "masa_mula" in self.cleaned_data:
+            if self.cleaned_data["masa_tamat"] <= self.cleaned_data["masa_mula"]:
                 raise forms.ValidationError(
                     "Masa tamat hendaklah selepas dari masa mula."
                 )
             return self.cleaned_data
 
-
         class Meta:
             model = SettingTempahan
             fields = "__all__"
             exclude = [
-
-
                 # TODO : tambah fields ni ke admin dan fix the functions
-                'max_tempahan_per_masa',
-                'max_tempahan_per_hari',
-        ]
+                "max_tempahan_per_masa",
+                "max_tempahan_per_hari",
+            ]
--- temujanji/views.py
+++ temujanji/views.py
@@ -1,10 +1,22 @@
 import datetime
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, View
-from temujanji.forms import TempahanMasaForm, TempahanSettingsForm, TempahanTarikhForm, TempahanPlangganForm
+from temujanji.forms import (
+    TempahanMasaForm,
+    TempahanSettingsForm,
+    TempahanTarikhForm,
+    TempahanPlangganForm,
+)
 from temujanji.models import Tempahan, SettingTempahan
-from temujanji.settings import PAGINATION,TEMUJANJI_BG,TEMUJANJI_DESC,TEMUJANJI_TAJUK, TEMUJANJI_DISABLE_URL, TEMUJANJI_SUCCESS_REDIRECT_URL
-from django.shortcuts import get_object_or_404,redirect, reverse, render
+from temujanji.settings import (
+    PAGINATION,
+    TEMUJANJI_BG,
+    TEMUJANJI_DESC,
+    TEMUJANJI_TAJUK,
+    TEMUJANJI_DISABLE_URL,
+    TEMUJANJI_SUCCESS_REDIRECT_URL,
+)
+from django.shortcuts import get_object_or_404, redirect, reverse, render
 from typing import Dict, List
 
 from formtools.wizard.views import SessionWizardView
@@ -18,7 +30,7 @@ from temujanji.utils import BookingSettingMixin
 #########
 
 
-class TempahanHomeView(BookingSettingMixin,TemplateView):
+class TempahanHomeView(BookingSettingMixin, TemplateView):
     model = Tempahan
     template_name = "temujanji/admin/dashboard.html"
 
@@ -36,28 +48,31 @@ class TempahanListView(BookingSettingMixin, ListView):
     template_name = "temujanji/admin/list_tempahan.html"
     paginate_by = PAGINATION
 
+
 class TempahanSettingView(BookingSettingMixin, UpdateView):
     form_class = TempahanSettingsForm
-    template_name = 'temujanji/admin/tempahan_settings.html'
+    template_name = "temujanji/admin/tempahan_settings.html"
 
     def get_object(self):
         return SettingTempahan.objects.filter().first()
 
     def get_success_url(self):
-        return reverse('tempahan_settings')
+        return reverse("tempahan_settings")
+
 
 class TempahanDeleteView(BookingSettingMixin, DeleteView):
     model = Tempahan
-    success_url = reverse_lazy('list_tempahan')
+    success_url = reverse_lazy("list_tempahan")
     queryset = Tempahan.objects.filter()
+
 
 class TempahanApproveView(BookingSettingMixin, View):
     model = Tempahan
-    success_url = reverse_lazy('list_tempahan')
-    fields = ('approved',)
+    success_url = reverse_lazy("list_tempahan")
+    fields = ("approved",)
 
     def post(self, request, *args, **kwargs):
-        tempahan = get_object_or_404(Tempahan, pk=self.kwargs.get('pk'))
+        tempahan = get_object_or_404(Tempahan, pk=self.kwargs.get("pk"))
         tempahan.approved = True
         tempahan.save()
 
@@ -67,85 +82,99 @@ class TempahanApproveView(BookingSettingMixin, View):
         # Part Untuk Book Temujanji
         #################
 
-BOOK_STEP_FORMS = (
-    ('Tarikh', TempahanTarikhForm),
-    ('Masa', TempahanMasaForm),
-    ('Info Pelanggan', TempahanPlangganForm)
 
+BOOK_STEP_FORMS = (
+    ("Tarikh", TempahanTarikhForm),
+    ("Masa", TempahanMasaForm),
+    ("Info Pelanggan", TempahanPlangganForm),
 )
 
+
 class CiptaTemujanjiWizardView(SessionWizardView):
-    template_name = 'temujanji/user/booking_wizard.html'
+    template_name = "temujanji/user/booking_wizard.html"
     form_list = BOOK_STEP_FORMS
 
     def get_context_data(self, form, **kwargs):
         context = super().get_context_data(form=form, **kwargs)
-        progress_width = '6'
-        if self.steps.current=='Masa':
-            context['dapatkan_masa_available'] = dapatkan_masa_available(self.get_cleaned_data_for_step('Tarikh')['tarikh'])
-            progress_width ='30'
-        if self.steps.current == 'Info Pelanggan':
-            progress_width = '75'
+        progress_width = "6"
+        if self.steps.current == "Masa":
+            context["dapatkan_masa_available"] = dapatkan_masa_available(
+                self.get_cleaned_data_for_step("Tarikh")["tarikh"]
+            )
+            progress_width = "30"
+        if self.steps.current == "Info Pelanggan":
+            progress_width = "75"
 
-        context.update({
-            'tempahan_settings' : SettingTempahan.objects.first(),
-            'progress_width': progress_width,
-            'booking_bg': TEMUJANJI_BG,
-            'description': TEMUJANJI_DESC,
-            'title' : TEMUJANJI_TAJUK
-        })
+        context.update(
+            {
+                "tempahan_settings": SettingTempahan.objects.first(),
+                "progress_width": progress_width,
+                "booking_bg": TEMUJANJI_BG,
+                "description": TEMUJANJI_DESC,
+                "title": TEMUJANJI_TAJUK,
+            }
+        )
         return context
 
-    def render(self,form=None, **kwargs):
-        #Chek jika temujanji Disable
+    def render(self, form=None, **kwargs):
+        # Check jika temujanji dimatikan/Disable
         form = form or self.get_form()
         context = self.get_context_data(form=form, **kwargs)
 
-        if not context['tempahan_settings'].booking_enable:
+        if not context["tempahan_settings"].tempahan_enable:
             return redirect(TEMUJANJI_DISABLE_URL)
 
         return self.render_to_response(context)
 
     def done(self, form_list, **kwargs):
-        data = dict((key, value) for form in form_list for key, value in form.cleaned_data.items())
+        data = dict(
+            (key, value)
+            for form in form_list
+            for key, value in form.cleaned_data.items()
+        )
         temujanji = Tempahan.objects.create(**data)
 
         if TEMUJANJI_SUCCESS_REDIRECT_URL:
             return redirect(TEMUJANJI_SUCCESS_REDIRECT_URL)
 
-        return render(self.request, 'temujanji/user/tempahan_done.html',{
-
-            'progress_width': '100',
-            'booking_id': temujanji.id,
-            'booking_bg' : TEMUJANJI_BG,
-            'descriptions' : TEMUJANJI_DESC,
-            'title': TEMUJANJI_TAJUK
-
-        } )
-
+        return render(
+            self.request,
+            "temujanji/user/tempahan_done.html",
+            {
+                "progress_width": "100",
+                "booking_id": temujanji.id,
+                "booking_bg": TEMUJANJI_BG,
+                "descriptions": TEMUJANJI_DESC,
+                "title": TEMUJANJI_TAJUK,
+            },
+        )
 
 
 def add_delta(masa: datetime.time, delta: datetime.datetime) -> datetime.time:
     # transform kepada full datetime first
-    return (datetime.datetime.combine(datetime.date.today(), time)+ delta).time()
+    return (datetime.datetime.combine(datetime.date.today(), time) + delta).time()
+
 
 def dapatkan_masa_available(date: datetime.date) -> List[Dict[datetime.time, bool]]:
-    '''
+    """
     Check semua availlable masa untuk pilih tarikh, masa mestilah antara masa_mula dan masa_tamat sekiranya masa sudah diambil -> is_taken = True
 
-    '''
+    """
 
     tempahan_settings = SettingTempahan.objects.first()
-    tempahan_sediaada = Tempahan.objects.filter(tarikh=tarikh).values_list('masa')
+    tempahan_sediaada = Tempahan.objects.filter(tarikh=tarikh).values_list("masa")
 
     next_time = tempahan_settings.masa_mula
     time_list = []
     while True:
         is_taken = any([x[0] == next_time for x in tempahan_sediaada])
         time_list.append(
-            {'time': ':'.join(str(next_time).split(':')[:-1]), "is_taken": is_taken}) 
-        next_time = add_delta(next_time, datetime.timedelta(
-            minutes=int(tempahan_settings.period_of_each_booking)))
+            {"time": ":".join(str(next_time).split(":")[:-1]), "is_taken": is_taken}
+        )
+        next_time = add_delta(
+            next_time,
+            datetime.timedelta(minutes=int(tempahan_settings.period_of_each_booking)),
+        )
         if next_time > tempahan_settings.masa_tamat:
             break
     return time_list
